4e7cb2b3ca97be398e8bfbea8fb7c5104c1a4fed
sam-verys <sam.park@verys.com>
2016-10-13 01:04:10 -0700

    adjust cors config

--- server/index.js
+++ server/index.js
@@ -41,9 +41,9 @@ app.use(session({
 }))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '127.0.0.1:8080')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next()
 })
 
