9205ddd55a8933ac2e2bbc3400898508e28bc58a
sam-verys <sam.park@verys.com>
2016-10-26 13:22:57 -0700

    d3 without library

--- app/components/UserTaste.jsx
+++ app/components/UserTaste.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { PieChart, Pie, Cell } from 'recharts'
+//import { PieChart, Pie, Cell } from 'recharts'
+import * as d3 from 'd3'
 
 class UserTaste extends Component {
   constructor (props) {
@@ -12,6 +13,39 @@ class UserTaste extends Component {
 
   componentDidMount () {
 
+    //<PieChart width={800} height={600}>
+    //      <Pie data={top10} cx='35%' cy='30%' outerRadius={100} innerRadius={80} labelLine={false} label={this.whichLabel} paddingAngle={6}>
+    //      {
+    //        top10.map((entry, index) => {
+    //          currColor = this.whichColor()
+    //          return <Cell key={`${entry.label}`} stroke={`${currColor}`} strokeWidth='1' fill='#363e42' onMouseEnter={this.showLabel} onMouseLeave={this.showLabel} />
+    //        })
+    //      }
+    //      </Pie>
+    //    </PieChart>
+  
+    let w = 800, h = 600, r = h / 2
+    let colors = ['#ff2b71', '#ff5ed2', '#3aa198', '#42d4ff', '#19647E', '#8B1E3F']
+
+    let svg = d3.select('body').select('svg')
+                .data([this.props.spotify.top10])
+                .attr('width', w)
+                .attr('height', h)
+                .append('svg:g')
+                .attr('transform', `translate(${r}, ${r})`)
+
+    let pie = d3.layout.pie().value((d) => d.value)
+    let arc = d3.svg.arc().outerRadius(r)
+
+    let arcs = svg.selectAll('g.slice')
+                  .data(pie)
+                  .enter()
+                  .append('svg:g')
+                  .attr('class', 'slice')
+
+    arcs.append('svg:path')
+        .attr('fill', (d, i) =>  colors[i % (colors.length - 1)])
+        .attr('d', (d) => arc(d))
   }
   
   render () {
@@ -21,16 +55,7 @@ class UserTaste extends Component {
     return (
       <div id='genresGraph'>
         <div className='header'><h3>Top Overall</h3></div>
-        <PieChart width={800} height={600}>
-          <Pie data={top10} cx='35%' cy='30%' outerRadius={100} innerRadius={80} labelLine={false} label={this.whichLabel} paddingAngle={6}>
-          {
-            top10.map((entry, index) => {
-              currColor = this.whichColor()
-              return <Cell key={`${entry.label}`} stroke={`${currColor}`} strokeWidth='1' fill='#363e42' onMouseEnter={this.showLabel} onMouseLeave={this.showLabel} />
-            })
-          }
-          </Pie>
-        </PieChart>
+        <svg></svg>
       </div>
     )
   }
